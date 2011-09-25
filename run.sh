c1b45b51ac275c0474e9a833552f48bb9baa395e
Stuart Andrews <stu.andrews@gmail.com>
2011-09-24 22:39:53 -0400

    Added octocat example

--- run.sh
+++ run.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-log="tub78-2011-09-21/LOG-2011-09-21.txt"
+dir=octocat-2011-09-24
+#dir="tub78-2011-09-21"
 
-cmd="python crawler.py --max_download 500 --max_distance 2 --sleep 500000 --download_dir tub78-2011-09-21"
+log="$dir/LOG.txt"
+
+cmd="python krwlr.py --max_download 10 --max_distance 2 --sleep 500000 --download_dir $dir"
+#cmd="python krwlr.py --max_download 500 --max_distance 2 --sleep 500000 --download_dir $dir"
 
 echo "echo $cmd >> $log"
 echo "$cmd 2>&1| tee -a $log"
